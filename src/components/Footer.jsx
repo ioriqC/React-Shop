6c6b6b90d599aa14725fdc180695cab8488c5e16
ioriqC <ioriqC@gmail.com>
2022-11-03 11:24:01 +0200

    gh pages added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   <div className="footer-copyright">
     <div className="container">
     © {new Date().getFullYear()} Copyright Text
-    <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+    <a className="grey-text text-lighten-4 right" href="https://ioriqc.github.io/React-Shop/" target="_blank" rel="noreferrer">Repo</a>
     </div>
   </div>
 </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 function Header() {
   return  <nav className="green lighten-2">
   <div className="nav-wrapper">
-    <a href="!#" className="brand-logo">React Shop</a>
+    <a href="/" className="brand-logo">React Shop</a>
     <ul id="nav-mobile" className="right hide-on-med-and-down">
-      <li><a href="!#">Repo</a></li>
+      <li><a href="https://ioriqc.github.io/React-Shop/" target="_blank" rel="noreferrer">Repo</a></li>
       
     </ul>
   </div>
